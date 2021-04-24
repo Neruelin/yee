c3195f54add6f8a84436f65a4644d4737f92dbeb
Tyler <neruelin@gmail.com>
2021-04-24 13:33:38 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import { useState, useRef, useEffect } from 'react';
 import './App.css';
 
-function App() {
+function Page({id, color, vw, vh}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id={id} style={{backgroundColor: color, width: vw, height: vh}}>
+      text
+    </div>
+  );
+}
+
+function App() {
+  const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+  const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+  const [index, setIndex] = useState(0);
+  const pages = [
+    {
+      color: "green",
+      vw: vw,
+      vh: vh,
+    }, {
+      color: "purple",
+      vw: vw,
+      vh: vh,
+    }  
+  ].map((val, ind) => (<Page id={ind} color={val.color} vw={val.vw} vh={val.vh}/>))
+
+  const scrool = (i) => {
+    document.getElementById(i).scrollIntoView({behavior: "smooth"});
+  };
+
+  useEffect(() => {
+    scrool(index);
+  }, [index])
+
+  return (
+    <div style={{width: vw, padding: 0, margin: 0, overflowX: "hidden"}}>
+      <div id="viewer" style={{display: "flex", width: 2*vw, padding: 0, margin: 0}}>
+        {pages}
+      </div>
+      <div style={{width: "100%", position: "absolute", bottom: 20, display: "flex", flexDirection: "row", justifyContent: "space-around"}}>
+        <div>
+          <a style={{padding: "0 10px", cursor: "pointer"}} onClick={() => setIndex(pages.length + index - 1 % pages.length)}>&lt;</a>
+          {
+            pages.map((val, ind) => (<a style={{padding: "0 10px", cursor: "pointer"}} onClick={() => setIndex(ind)}>O</a>))
+          }
+          <a style={{padding: "0 10px", cursor: "pointer"}} onClick={() => setIndex(pages.length + index + 1 % pages.length)}>&gt;</a>
+        </div>
+      </div>
     </div>
   );
 }
