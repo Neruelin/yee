09c517fc167f6784983410db94cfdc621f9f6948
Tyler <neruelin@gmail.com>
2021-04-24 14:03:59 -0400

    better controls"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { useState, useRef, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight, faArrowLeft, faCircle, faDotCircle, faHome } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 
 function Page({id, color, vw, vh}) {
@@ -22,11 +24,14 @@ function App() {
       color: "purple",
       vw: vw,
       vh: vh,
+    }, {
+      color: "red",
+      vw: vw,
+      vh: vh,
     }  
   ].map((val, ind) => (<Page id={ind} color={val.color} vw={val.vw} vh={val.vh}/>))
 
   const scrool = (i) => {
-    console.log(i)
     document.getElementById(i).scrollIntoView({behavior: "smooth"});
   };
 
@@ -36,16 +41,20 @@ function App() {
 
   return (
     <div style={{width: vw, padding: 0, margin: 0, overflowX: "hidden"}}>
-      <div id="viewer" style={{display: "flex", width: 2*vw, padding: 0, margin: 0}}>
+      <div id="viewer" style={{display: "flex", width: pages.length*vw, padding: 0, margin: 0}}>
         {pages}
       </div>
-      <div style={{width: "100%", position: "absolute", bottom: 20, display: "flex", flexDirection: "row", justifyContent: "space-around"}}>
+      <div style={{width: "100%", position: "absolute", bottom: 40, display: "flex", flexDirection: "row", justifyContent: "space-around"}}>
         <div>
-          <a style={{padding: "0 10px", cursor: "pointer"}} onClick={() => setIndex((pages.length + index - 1) % pages.length)}>&lt;</a>
-          {
-            pages.map((val, ind) => (<a style={{padding: "0 10px", cursor: "pointer"}} onClick={() => setIndex(ind)}>O</a>))
-          }
-          <a style={{padding: "0 10px", cursor: "pointer"}} onClick={() => setIndex((pages.length + index + 1) % pages.length)}>&gt;</a>
+          <a class="controlIcon" onClick={() => setIndex((pages.length + index - 1) % pages.length)}>
+            <FontAwesomeIcon icon={faArrowLeft} />
+          </a>
+          {pages.map((val, ind) => (<a class="controlIcon" onClick={() => setIndex(ind)}>
+            <FontAwesomeIcon icon={(ind == 0) ? faHome : (ind == index) ? faDotCircle : faCircle} color={(ind == index ? "white" : "black")}/>
+          </a>))}
+          <a class="controlIcon" onClick={() => setIndex((pages.length + index + 1) % pages.length)}>
+            <FontAwesomeIcon icon={faArrowRight} onMouse />
+          </a>
         </div>
       </div>
     </div>
